d0c9eff4d5b65df90fbea827d003170329052a82
gevgeny <e.gluhotorenko@gmail.com>
2019-12-15 18:45:32 +0300

    Initial Commit

---
+++ Babbler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Babbler
+//
+//  Created by Eugene Gluhotorenko on 12/15/19.
+//  Copyright © 2019 Eugene Gluhotorenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
